d58448be25196d383f9909c3bf914479ea85cc4a
iriska09 <kudaibergenovayryska09@gmail.com>
2024-02-27 05:44:04 +0000

    session-1.py

---
+++ session1.py
@@ -0,0 +1,16 @@
+inp1 = int(input("What is you number: "))
+
+inp2 = int(input("What is your number: "))
+
+print(inp1 + inp2)
+
+
+
+##task2
+
+number1=10.5
+number2=20.5
+print ( number1 - number2 )
+print ( number1 + number2 )
+print ( number1 * number2 )
+print ( number1 // number2 )
