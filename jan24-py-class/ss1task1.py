e0364b4334ff42f7cc609929dcb6155fab1b5add
iriska09 <kudaibergenovayryska09@gmail.com>
2024-03-04 21:25:38 +0000

    reduing hw

--- jan24-py-class/ss1task1.py
+++ jan24-py-class/ss1task1.py
@@ -1,8 +1,12 @@
-def reverse (string) :
-    string = string [::-1]
-    return string 
-s = "Hello World!"
-print ("The original string is : " '')
-print (s)
-print ("The reversed string  is :" '')
-print (reverse(s))
+# def reverse (string) :
+#     string = string [::-1]
+#     return string 
+# s = "Hello World!"
+# print ("The original string is : " '')
+# print (s)
+# print ("The reversed string  is :" '')
+# print (reverse(s))
+
+######## shorter version ##### reduing HW
+s = " Hello world !"
+print(s[::-1])
