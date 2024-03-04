5acb5b2655ecf1dae11149fb7e95e869a0001903
iriska09 <kudaibergenovayryska09@gmail.com>
2024-03-04 21:14:06 +0000

    reduing hw

--- jan24-py-class/ss1task4.py
+++ jan24-py-class/ss1task4.py
@@ -3,5 +3,5 @@ inp2 = str(input("What is your sevice ?: "))
 inp3 = str(input("What is your region ?: "))
 inp4 = int(input("What is your account-id?: "))
 inp5 = int(input("What is your resource-id?: "))
-print ("arn:inp1:inp2:inp3:inp4:inp5")
-
+output_str = "arn:" + inp1 + ":" + inp2 + ":" + inp3 + ":" + str(inp4) + ":" + str(inp5)
+print(output_str)
