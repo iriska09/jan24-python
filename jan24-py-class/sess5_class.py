48c62718d336b59c3d54ffb8b0518265b9b57d6a
iriska09 <kudaibergenovayryska09@gmail.com>
2024-03-09 16:30:13 +0000

    session5

---
+++ jan24-py-class/sess5_class.py
@@ -0,0 +1,114 @@
+def converter(celsius):
+    f = (celsius * 1.8)+ 32
+    retern(f)
+inp = int(input("provide your degree: "))
+print (converter)
+
+
+###
+
+def oddnum(par):
+    odd_list = []
+    n = (num for num in par if  num % 2 != 0 )
+    odd_list.append(num)
+    return odd_list
+
+
+num = [1,2,3,4,5,6,7,8,9,10]
+print(oddnum(num))
+
+
+def number1()
+    n = 1 - 2
+    return n
+def number2()
+    b = 2 - 4
+
+print(number1() - number2())
+
+
+def checking(lst,char):
+    word_lst = []
+    for char in word:
+        word_lst.append(word)
+    
+    return word_lst
+
+fruits = ["banana","apple","cherry","mango"]
+# print(checking(lst,char "a"))
+
+
+
+
+def add_num(num1,num2):
+    return num1 + num2
+
+
+num1 = 34
+num2 =20
+print(add_num(num1,num2))
+
+
+
+
+def add_num(num1,num2):
+    return num1 - num2
+
+num1 = 34
+num2 =20
+print(add_num(num1,num2))
+
+
+def check(word, char):
+    if char in word:
+        return word
+    else:
+        return char
+
+print(check("Hello","e"))
+print(check("[caf, bottle, something","$"))
+
+variable = "Hello"
+char = "e"
+print (check("Hello")) 
+
+write a function that will take an input as a string  and reverse it 
+take an input with prompt 
+
+
+def add_num(num1,num2):
+    return num1 + num2
+
+
+num1 = 34
+num2 =20
+print(add_num(num1,num2))
+
+def revers_str(word):
+    word = word [::-1]
+    return word
+
+# word = "Hello world"
+# print(revers_str(word))
+
+
+def calculate_area(radius,p):
+    return radius * p 
+
+radius = 5
+p = 3.14
+print(calculate_area(radius,p))
+
+# x = 4
+
+# y = 6
+
+# z = 4
+
+# x = 8
+
+# y = 7
+
+# print (x, y)
+
+
