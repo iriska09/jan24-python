c94e225e45e197650bf32046830a17cca0813738
iriska09 <kudaibergenovayryska09@gmail.com>
2024-03-09 16:31:06 +0000

    testing sessions

--- jan24-py-class/test.py
+++ jan24-py-class/test.py
@@ -55,3 +55,55 @@ for i in range(1,4):
     for x in range(1,4):
         for j in range(1,4):
                 print(i,x,j)
+mytuples = ("cherry", "apple", "banana", "banana", ["21312","8694397","314-241"])
+print (mytuples.count("banana"))
+for elem in mytuples:
+    print (elem)
+####### here we can change we have list in the tuples 
+person = ("Yrys", "Bolotbek", "08.05.02", "123-456", ["21312423","8694397","314-241"])
+person[-1][0] = "989-99"
+print (person)
+########## tuples we can not change but if we have a string inside of the tuples we 
+we can change the string 
+lst = ["21312423", ("Yrys", "Bolotbek", "08.05.02", "123-456", )]
+(lst[0]) = "5448-35"
+print(lst)
+person = {
+"name" : ("Yrys",), 
+"lastname": "Bolotbek", 
+"dob": "08.05.02",
+"number": ["123-456","4526-5375"]
+}
+# person["name"] [0]
+person["name"] = "Khusrav"
+
+# print(person)
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+task 4
+
+connections = [
+    ('Amsterdam', 'Dublin', 100),
+    ('Amsterdam', 'Rome', 140),
+    ('Rome', 'Warsaw', 130),
+    ('Minsk', 'Prague', 95),
+    ('Stockholm', 'Rome', 190),
+    ('Copenhagen', 'Paris', 120),
+    ('Madrid', 'Rome', 135),
+    ('Lisbon', 'Rome', 170),
+    ('Dublin', 'Rome', 170),
+    ]
+rome_how_many = [elem for elem in connections if elem[1] == 'Rome']
+print (len(rome_how_many))
+time_finding = sum(elem[2] for elem in rome_how_many) / len(rome_how_many)
+print (time_finding)
+print(f"{len(rome_how_many)} here is the time {time_finding} minutes")
+
+
+
+
